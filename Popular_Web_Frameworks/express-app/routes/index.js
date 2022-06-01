04f63e4ea5666fbee1e58a875c6536c6ec1a8606
Or Hasson <hassonor@gmail.com>
2022-06-01 12:59:43 +0300

    Add basic express app

---
+++ Popular_Web_Frameworks/express-app/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    const title = "Express :)";
+    res.send(`<html><head><title>${title}</title><link rel="stylesheet" href="style.css"></head><body>
+    <h1> ${title}</h1>
+    <p> Welcome to ${title}</p>
+</body></html>`);
+})
+
+module.exports = router;
